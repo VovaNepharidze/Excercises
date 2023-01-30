cbaba7c2ed2430c1e9cc7dbbd86d4c6fef3a7984
vova nefaridze <vova_n@yahoo.com>
2023-01-30 19:03:55 +0400

    modified fourt exercise

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 'use strict';
 
 
-function simpleCalculator (){
-    let number1=  Number(prompt("please enter first number:"))
-    let mathOperation = prompt("please choose math operation: + - * / ")
-    let number2=  Number(prompt("please enter second number:"))
+// function simpleCalculator (){
+//     let number1=  Number(prompt("please enter first number:"))
+//     let mathOperation = prompt("please choose math operation: + - * / ")
+//     let number2=  Number(prompt("please enter second number:"))
     
-        if(mathOperation === '+'){
-        return number1 + number2
-    }else if(mathOperation === '-'){
-        return number1 - number2
-    }else if(mathOperation === '/'){
-        return number1 / number2
-    }else if(mathOperation === '*'){
-        return number1 * number2
-    }
-}
+//         if(mathOperation === '+'){
+//         return number1 + number2
+//     }else if(mathOperation === '-'){
+//         return number1 - number2
+//     }else if(mathOperation === '/'){
+//         return number1 / number2
+//     }else if(mathOperation === '*'){
+//         return number1 * number2
+//     }
+// }
 
-const result = simpleCalculator()
-console.log(`the result of mathematical operation you choose is : ${result}`)
+// const result = simpleCalculator()
+// console.log(`the result of mathematical operation you choose is : ${result}`)
 
 
-function removeSpaces(array){
-    let newarray =""
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] !== " "){
-            newarray += array[i]
-        }
-    }return newarray
+// function removeSpaces(array){
+//     let newarray =""
+//     for (let i = 0; i < array.length; i++) {
+//         if(array[i] !== " "){
+//             newarray += array[i]
+//         }
+//     }return newarray
 
-}
+// }
 
-let array = 'hello My crazy world'
-console.log(removeSpaces(array))
+// let array = 'hello My crazy world'
+// console.log(removeSpaces(array))
 
 
 //////find shortests strings//////
 
-function findShortestString(arr){
-    let shorteststring = [arr[0]];
-    let shortestLength = arr[0].length;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i].length < shortestLength ){
-            shorteststring = [arr[i]];
-            shortestLength = arr[i].length
-        }else if(arr[i].length === shortestLength){
-            shorteststring.push(arr[i])
-        }
-    }return shorteststring;
-}
+// function findShortestString(arr){
+//     let shorteststring = [arr[0]];
+//     let shortestLength = arr[0].length;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i].length < shortestLength ){
+//             shorteststring = [arr[i]];
+//             shortestLength = arr[i].length
+//         }else if(arr[i].length === shortestLength){
+//             shorteststring.push(arr[i])
+//         }
+//     }return shorteststring;
+// }
 
 
-const array2 = ['levani','anuki','qeti','guga']
-console.log(findShortestString(array2))
+// const array2 = ['levani','anuki','qeti','guga']
+// console.log(findShortestString(array2))
 
 
 // fourth exercise//
@@ -62,12 +62,12 @@ function reverseArray(arr){
 }
 
 let array1 = [1,2,3]
-console.log(reverseArray(array))
+console.log(reverseArray(array1))
 
 
 // fourth exercise (2) //
 
-function reverseArray(arr){
+function reverseArray1(arr){
     let arr2=[];
     for (let i = 0; i < arr.length; i++) {
         arr2.unshift(arr[i]);
@@ -75,20 +75,20 @@ function reverseArray(arr){
 }
 
 const array3 = [0,1,2,3]
-const newArray = reverseArray(array)
+const newArray = reverseArray1(array3)
 console.log(newArray)
 
 
-function array(arr){
-    let sum=0;
+// function array(arr){
+//     let sum=0;
 
-    for (let i = 0; i < arr.length; i++){
-        sum = sum +arr[i]
-    }
+//     for (let i = 0; i < arr.length; i++){
+//         sum = sum +arr[i]
+//     }
 
-    return sum;
-}
+//     return sum;
+// }
 
-const numbers=[5,10,15,20]
-const sum = array(numbers)
-console.log(`The sum of all the numbers in the array is : ${sum}`)
+// const numbers=[5,10,15,20]
+// const sum = array(numbers)
+// console.log(`The sum of all the numbers in the array is : ${sum}`)
