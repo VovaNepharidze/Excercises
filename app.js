6cf0dc38cbfc7b6f583e388cd36e930967f7820b
vova nefaridze <vova_n@yahoo.com>
2023-01-30 19:08:14 +0400

    modified file

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 'use strict';
 
 
-// function simpleCalculator (){
-//     let number1=  Number(prompt("please enter first number:"))
-//     let mathOperation = prompt("please choose math operation: + - * / ")
-//     let number2=  Number(prompt("please enter second number:"))
+function simpleCalculator (){
+    let number1=  Number(prompt("please enter first number:"))
+    let mathOperation = prompt("please choose math operation: + - * / ")
+    let number2=  Number(prompt("please enter second number:"))
     
-//         if(mathOperation === '+'){
-//         return number1 + number2
-//     }else if(mathOperation === '-'){
-//         return number1 - number2
-//     }else if(mathOperation === '/'){
-//         return number1 / number2
-//     }else if(mathOperation === '*'){
-//         return number1 * number2
-//     }
-// }
+        if(mathOperation === '+'){
+        return number1 + number2
+    }else if(mathOperation === '-'){
+        return number1 - number2
+    }else if(mathOperation === '/'){
+        return number1 / number2
+    }else if(mathOperation === '*'){
+        return number1 * number2
+    }
+}
 
-// const result = simpleCalculator()
-// console.log(`the result of mathematical operation you choose is : ${result}`)
-
-
-// function removeSpaces(array){
-//     let newarray =""
-//     for (let i = 0; i < array.length; i++) {
-//         if(array[i] !== " "){
-//             newarray += array[i]
-//         }
-//     }return newarray
-
-// }
-
-// let array = 'hello My crazy world'
-// console.log(removeSpaces(array))
+const result = simpleCalculator()
+console.log(`the result of mathematical operation you choose is : ${result}`)
 
 
-//////find shortests strings//////
+function removeSpaces(array){
+    let newarray =""
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] !== " "){
+            newarray += array[i]
+        }
+    }return newarray
 
-// function findShortestString(arr){
-//     let shorteststring = [arr[0]];
-//     let shortestLength = arr[0].length;
-//     for (let i = 1; i < arr.length; i++) {
-//         if (arr[i].length < shortestLength ){
-//             shorteststring = [arr[i]];
-//             shortestLength = arr[i].length
-//         }else if(arr[i].length === shortestLength){
-//             shorteststring.push(arr[i])
-//         }
-//     }return shorteststring;
-// }
+}
+
+let array = 'hello My crazy world'
+console.log(removeSpaces(array))
 
 
-// const array2 = ['levani','anuki','qeti','guga']
-// console.log(findShortestString(array2))
+////find shortests strings//////
+
+function findShortestString(arr){
+    let shorteststring = [arr[0]];
+    let shortestLength = arr[0].length;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length < shortestLength ){
+            shorteststring = [arr[i]];
+            shortestLength = arr[i].length
+        }else if(arr[i].length === shortestLength){
+            shorteststring.push(arr[i])
+        }
+    }return shorteststring;
+}
+
+
+const array2 = ['levani','anuki','qeti','guga']
+console.log(findShortestString(array2))
 
 
 // fourth exercise//
@@ -79,16 +79,16 @@ const newArray = reverseArray1(array3)
 console.log(newArray)
 
 
-// function array(arr){
-//     let sum=0;
+function arraySum(arr){
+    let sum=0;
 
-//     for (let i = 0; i < arr.length; i++){
-//         sum = sum +arr[i]
-//     }
+    for (let i = 0; i < arr.length; i++){
+        sum = sum +arr[i]
+    }
 
-//     return sum;
-// }
+    return sum;
+}
 
-// const numbers=[5,10,15,20]
-// const sum = array(numbers)
-// console.log(`The sum of all the numbers in the array is : ${sum}`)
+const numbers=[5,10,15,20]
+const sum = arraySum(numbers)
+console.log(`The sum of all the numbers in the array is : ${sum}`)
